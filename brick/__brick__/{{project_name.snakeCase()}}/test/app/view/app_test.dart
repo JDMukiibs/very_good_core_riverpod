68ee43cf68233fb04db385b47d53d4a00a2a5cd7
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2023-08-31 18:42:47 +0300

    make: final changes to brick

--- brick/__brick__/{{project_name.snakeCase()}}/test/app/view/app_test.dart
+++ brick/__brick__/{{project_name.snakeCase()}}/test/app/view/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:{{project_name.snakeCase()}}/app/app.dart';
 import 'package:{{project_name.snakeCase()}}/counter/counter.dart';
@@ -6,8 +7,8 @@ void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(
-        ProviderScope(
-          child: const App(),
+        const ProviderScope(
+          child: App(),
         ),
       );
       expect(find.byType(CounterPage), findsOneWidget);
